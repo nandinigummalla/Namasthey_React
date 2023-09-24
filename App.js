b28540bd668992d2748cb9052bd65e0df2a6c385
nandini gummalla <68344564+nandinigummalla@users.noreply.github.com>
2023-09-24 21:50:22 +0530

    second episode

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello World using React!!!");
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World using React with parcel !!!");
 // const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "Hie I m h1 tag with parent child combo")))
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "Hie I m h1 tag with siblings"), React.createElement("h2", {}, "Hie I m h2 tag with siblings")]))
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "Hie I m h1 tag with siblings !!"), React.createElement("h2", {}, "Hie I m h2 tag with siblings & using parcel!!")]))
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 
--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
 #Namasthey react _ Inception ( 1st episode )
+
+npm init
+npm i -D parcel
+npx parcel index.html  -- to run
+npm install react
+npm i react-dom
+npx parcel build index.html -- building prod app ( remove main: app.js in package.json file)
