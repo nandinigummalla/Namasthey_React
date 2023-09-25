2855edb24954715ab236e96b65fe0db11a8270d5
nandini gummalla <68344564+nandinigummalla@users.noreply.github.com>
2023-09-26 00:27:52 +0530

    episode 3 part 3

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World using React with parcel !!!");
-// const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "Hie I m h1 tag with parent child combo")))
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "Hie I m h1 tag with siblings !!"), React.createElement("h2", {}, "Hie I m h2 tag with siblings & using parcel!!")]))
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// React.createElement --> React Element -- js object  --> HTMl Element (render)
+// const heading = React.createElement("h1", { id: "heading" }, "Namasthey React");   // As it's difficult to write will use jsx code from now
 
-// createElement is a object  and when we render it then the it will convert to html tag and pass to root element
+// jsx code is html like code and will have minor differences compared to html.. At the end jsx code converts to React code ( becoz js engines don't understand jsx)
+const heading = <h1 id="heading">Namasthey React</h1>   // This will be transpiled to React code( React.createElement) with the help of Babel 
+
+const multiLineHeading = (<h1 className="head">
+    Namasthey
+    React
+    from Nandini
+</h1>);  // Need to be binded in () when we write in multiple lines
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(multiLineHeading)  // On render the js object converts to html element
--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@
 
 npm init
 npm i -D parcel
-npx parcel index.html  -- to run
+npx parcel index.html  -- to build and run for development   -- (From now will use these commands to run ) -- npm run start, npm start 
 npm install react
 npm i react-dom
-npx parcel build index.html -- building prod app ( remove main: app.js in package.json file)
+npx parcel build index.html -- building prod app ( remove main: app.js in package.json file)  -- (From now will use these commands to run ) -- npm run build
